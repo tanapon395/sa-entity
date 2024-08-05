360c02ba3b8c82b64309940a07039918834253ae
tanapon395 <tanapon395@gmail.com>
2024-08-05 13:54:47 +0700

    update field

--- entity/member.go
+++ entity/member.go
@@ -1,12 +1,19 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Member struct {
 	gorm.Model
-	Username string
-	Password string
-	Email    string
+	Username  string
+	Password  string
+	Email     string
+	FirstName string
+	LastName  string
+	Birthday  time.Time
 
 	// 1 member เป็นเจ้าของได้หลาย Playlist
 	Playlists []Playlist `gorm:"foreignKey:MemberID"`
